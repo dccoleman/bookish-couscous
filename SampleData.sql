f8aaa01e03a680831952db032eedc6e6bd9154c5
dccoleman <dccoleman@wpi.edu>
2016-02-15 14:48:10 -0500

    Starting Sample Data Generation
    As title. Filling the Guide and Vehicle tables with info, will fill
    more once all relations are created and checks implemented

---
+++ SampleData.sql
@@ -0,0 +1,24 @@
+/* Inserting Sample Data into Tables */
+--Guides
+INSERT INTO Guide (DriverLicense,FirstName,LastName,Phone,VehicleType,Title,Salary)
+VALUES (1111111111111,'One','Un',1111111111,'Car','Junior Guide',1);
+INSERT INTO Guide (DriverLicense,FirstName,LastName,Phone,VehicleType,Title,Salary)
+VALUES (2222222222222,'Two','Deux',2222222222,'Amphibious','Senior Guide',2);
+INSERT INTO Guide (DriverLicense,FirstName,LastName,Phone,VehicleType,Title,Salary)
+VALUES (3333333333333,'Three','Trois',3333333333,'Bus','Guide',3);
+INSERT INTO Guide (DriverLicense,FirstName,LastName,Phone,VehicleType,Title,Salary)
+VALUES (4444444444444,'Four','Quatre',2222222222,'Amphibious','Guide',4);
+INSERT INTO Guide (DriverLicense,FirstName,LastName,Phone,VehicleType,Title,Salary)
+VALUES (5555555555555,'Five','Cinq',5555555555,'Bus','Senior Guide',5);
+
+--Vehicles
+INSERT INTO Vehicle (LicensePlate,VehicleType,VehicleMake,VehicleModel,VehicleYear,MaxPassenger)
+VALUES (1111111,'Car','Ford','Mustang',1995,4);
+INSERT INTO Vehicle (LicensePlate,VehicleType,VehicleMake,VehicleModel,VehicleYear,MaxPassenger)
+VALUES (2222222,'Bus','Mystery','Machine',1984,4);
+INSERT INTO Vehicle (LicensePlate,VehicleType,VehicleMake,VehicleModel,VehicleYear,MaxPassenger)
+VALUES (3333333,'Amphibious','Scrooge','McDuckMobile',1897,36);
+INSERT INTO Vehicle (LicensePlate,VehicleType,VehicleMake,VehicleModel,VehicleYear,MaxPassenger)
+VALUES (4444444,'Bus','Rolls Royce','WealthMobile',1993,2);
+INSERT INTO Vehicle (LicensePlate,VehicleType,VehicleMake,VehicleModel,VehicleYear,MaxPassenger)
+VALUES (5555555,'Car','Dodge','Hellcat',2015,4);
