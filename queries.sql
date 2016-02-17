b581cfba23dde862f9026a6496e3a510d37d52c9
Troy Hughes <Troy@THHUGHES27.WIFI.WPI.EDU>
2016-02-17 16:27:15 -0500

    Trying to get some queries going

---
+++ queries.sql
@@ -0,0 +1,16 @@
+-- 1 
+SELECT LicensePlate, VehicleMake, VehicleModel 
+FROM Vehicle 
+WHERE MaxPassenger > 10;
+
+-- 2 
+SELECT FirstName, LastName, LicensePlate, VehicleMake, VehicleModel
+FROM Vehicle Ve, Guide Gu
+WHERE Ve.VehicleType = Gu.VehicleType
+AND Gu.DriverLicense = 1111111111111
+
+-- 3
+SELECT Cu.CustomerID, Cu.FirstName, Cu.LastName, Cu.Age, COUNT(Bt.BookedTourID) AS cntBT
+FROM Customer Cu, BookedTour Bt,
+WHERE Cu.CustomerID = Bt.CustomerID
+GROUP BY Cu.CustomerID, Cu.FirstName, Cu.LastName, Cu.Age
