27223388721f84639e91f6d8b947e0ade4f023b7
dccoleman <dccoleman@wpi.edu>
2016-02-17 22:09:52 -0500

    Added some files
    Added report.pdf as requested by the assignment, will submit zip next.

--- FirstTable.sql
+++ FirstTable.sql
@@ -1,3 +1,5 @@
+/* Part 1 */
+
 /* Dropping all tables */
 DROP TABLE Traveling;
 DROP TABLE Vehicle;
@@ -8,7 +10,7 @@ DROP TABLE Customer;
 DROP TABLE Guide;
 DROP TABLE Tour;
 
-/* Creating tables*/
+/* Creating tables */
 CREATE TABLE Guide
 (
 DriverLicense varchar(13) CONSTRAINT Guide_PK PRIMARY KEY,
@@ -103,3 +105,241 @@ FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
 FOREIGN KEY (DriverLicense) REFERENCES Guide(DriverLicense)
 );
 
+
+/* This is a question from part 3, but it's here so that it takes effect
+for each addition of our sampledata to the table. */
+/* Q10: For a specific booked tour, insert the TotalPrice by
+calculating $100 per adult ( >= 18 yrs) and $50 per child (<18 yrs). */
+CREATE TRIGGER EvalBookedTourTotal
+BEFORE INSERT ON BookedTour
+FOR EACH ROW 
+DECLARE
+	cursor curs1(CID in VARCHAR) is
+		(SELECT TravelingWith.Age
+		FROM TravelingWith
+		WHERE TravelingWith.CustomerID = CID)
+		
+		UNION
+		
+		(SELECT Customer.Age
+		FROM Customer
+		WHERE CID = Customer.CustomerID);
+BEGIN 
+:new.TotalPrice :=0;
+	FOR currentAge in curs1(:new.CustomerID) LOOP
+		IF (currentAge.Age < 18) THEN 
+			:new.TotalPrice := :new.TotalPrice + 50;
+		ELSE :new.TotalPrice := :new.TotalPrice + 100;
+		END IF;
+	END LOOP;
+END; 
+/
+
+	
+	
+
+/* Part 2 */
+/* Inserting Sample Data into Tables */
+--Guides
+INSERT INTO Guide (DriverLicense,FirstName,LastName,Phone,VehicleType,Title,Salary,HireDate)
+VALUES (1111111111111,'One','Un',1111111111,'Car','Junior Guide',1,'01-JAN-2016');
+INSERT INTO Guide (DriverLicense,FirstName,LastName,Phone,VehicleType,Title,Salary,HireDate)
+VALUES (2222222222222,'Two','Deux',2222222222,'Amphibious','Senior Guide',2,'02-FEB-2016');
+INSERT INTO Guide (DriverLicense,FirstName,LastName,Phone,VehicleType,Title,Salary,HireDate)
+VALUES (3333333333333,'Three','Trois',3333333333,'Bus','Guide',3,'03-MAR-2000');
+INSERT INTO Guide (DriverLicense,FirstName,LastName,Phone,VehicleType,Title,Salary,HireDate)
+VALUES (4444444444444,'Four','Quatre',2222222222,'Amphibious','Guide',4,'04-APR-2012');
+INSERT INTO Guide (DriverLicense,FirstName,LastName,Phone,VehicleType,Title,Salary,HireDate)
+VALUES (5555555555555,'Five','Cinq',5555555555,'Bus','Senior Guide',5,'05-MAR-1995');
+
+--Vehicles
+INSERT INTO Vehicle (LicensePlate,VehicleType,VehicleMake,VehicleModel,VehicleYear,MaxPassenger)
+VALUES (1111111,'Car','Ford','Mustang','05-DEC-1996',4);
+INSERT INTO Vehicle (LicensePlate,VehicleType,VehicleMake,VehicleModel,VehicleYear,MaxPassenger)
+VALUES (2222222,'Bus','Mystery','Machine','12-JAN-2001',4);
+INSERT INTO Vehicle (LicensePlate,VehicleType,VehicleMake,VehicleModel,VehicleYear,MaxPassenger)
+VALUES (3333333,'Amphibious','Scrooge','McDuckMobile','16-AUG-2002',36);
+INSERT INTO Vehicle (LicensePlate,VehicleType,VehicleMake,VehicleModel,VehicleYear,MaxPassenger)
+VALUES (4444444,'Bus','Rolls Royce','WealthMobile','30-MAY-1995',2);
+INSERT INTO Vehicle (LicensePlate,VehicleType,VehicleMake,VehicleModel,VehicleYear,MaxPassenger)
+VALUES (5555555,'Car','Dodge','Hellcat','30-JAN-1000',4);
+
+--Tours
+INSERT INTO Tour (TourID, TourName, Description, City, StateHeld, Duration, VehicleType, AdultCost, ChildCost)
+VALUES (1111111,'Tour A', 'Words', 'Worcester', 'MA', 7, 'Car', 10, 3);
+INSERT INTO Tour (TourID, TourName, Description, City, StateHeld, Duration, VehicleType, AdultCost, ChildCost)
+VALUES (2222222,'Tour B', 'Words', 'D.C.', 'D.C.', 7, 'Amphibious', 10, 3);
+INSERT INTO Tour (TourID, TourName, Description, City, StateHeld, Duration, VehicleType, AdultCost, ChildCost)
+VALUES (0000000,'Tour C', 'Words', 'Maryland', 'MA', 7, 'Bus', 10, 3);
+INSERT INTO Tour (TourID, TourName, Description, City, StateHeld, Duration, VehicleType, AdultCost, ChildCost)
+VALUES (3333333,'Tour D', 'Words', 'Boston', 'MA', 7, 'Car', 10, 3);
+INSERT INTO Tour (TourID, TourName, Description, City, StateHeld, Duration, VehicleType, AdultCost, ChildCost)
+VALUES (4444444,'Tour E', 'Words', 'Detroit', 'MI', 7, 'Car', 10, 3);
+INSERT INTO Tour (TourID, TourName, Description, City, StateHeld, Duration, VehicleType, AdultCost, ChildCost)
+VALUES (5555555,'Tour F', 'Words', 'Las Vegas', 'Nevada', 7, 'Amphibious', 10, 3);
+
+--Location
+INSERT INTO Location (LocationID, LocationName, LocationType, LocationAddress, Longitude, Latitude)
+VALUES (1111111, 'Some Place', 'Restaurant','1234 somewhere street',7,5);
+INSERT INTO Location (LocationID, LocationName, LocationType, LocationAddress, Longitude, Latitude)
+VALUES (2222222, 'The Moon', 'Historic','Space',8,3);
+INSERT INTO Location (LocationID, LocationName, LocationType, LocationAddress, Longitude, Latitude)
+VALUES (3333333, 'Death Star', 'Museum','Really Far Away',9,8);
+INSERT INTO Location (LocationID, LocationName, LocationType, LocationAddress, Longitude, Latitude)
+VALUES (4444444, 'Forest Moon of Endor', 'Historic','Outer Rim',10,5);
+INSERT INTO Location (LocationID, LocationName, LocationType, LocationAddress, Longitude, Latitude)
+VALUES (5555555, 'Worcester', 'Restaurant','Middle of MA',11,111);
+
+-- Customer 
+INSERT INTO Customer (CustomerID, FirstName, LastName, Address, Phone, Age)
+VALUES (1111111, 'John', 'Jacob jingle heimer schmitt','Drewry Lane',2223334444,7);
+INSERT INTO Customer (CustomerID, FirstName, LastName, Address, Phone, Age)
+VALUES (2222222, 'Jake', 'brown','100 Institute Rd. ',2223334444,7);
+INSERT INTO Customer (CustomerID, FirstName, LastName, Address, Phone, Age)
+VALUES (3333333, 'Alex', 'brown','100 Institute Rd. ',3334445555,7);
+INSERT INTO Customer (CustomerID, FirstName, LastName, Address, Phone, Age)
+VALUES (4444444, 'Ben', 'Stevens','20 Dix Street',4445556666,7);
+INSERT INTO Customer (CustomerID, FirstName, LastName, Address, Phone, Age)
+VALUES (5555555, 'Stan', 'van Zyl','21 Dix Street',5556667777,7);
+INSERT INTO Customer (CustomerID, FirstName, LastName, Address, Phone, Age)
+VALUES (6666666, 'Shirly', 'Price','22 Dix Street',6667778888,7);
+
+--TravelingWith
+INSERT INTO TravelingWith (TravelingWithID, CustomerID, FirstName, LastName, Age)
+VALUES (2222222, 1111111, 'John', 'Jacob jingle heimer schmitt',7);
+INSERT INTO TravelingWith (TravelingWithID, CustomerID, FirstName, LastName, Age)
+VALUES (3333333, 1111111, 'John', 'Jacob jingle heimer schmitt',7);
+INSERT INTO TravelingWith (TravelingWithID, CustomerID, FirstName, LastName, Age)
+VALUES (4444444, 1111111, 'John', 'Jacob jingle heimer schmitt',7);
+INSERT INTO TravelingWith (TravelingWithID, CustomerID, FirstName, LastName, Age)
+VALUES (5555555, 1111111, 'John', 'Jacob jingle heimer schmitt',7);
+INSERT INTO TravelingWith (TravelingWithID, CustomerID, FirstName, LastName, Age)
+VALUES (6666666, 1111111, 'John', 'Jacob jingle heimer schmitt',7);
+
+INSERT INTO TravelingWith (TravelingWithID, CustomerID, FirstName, LastName, Age)
+VALUES (3333333, 2222222, 'Jake', 'brown',7);
+INSERT INTO TravelingWith (TravelingWithID, CustomerID, FirstName, LastName, Age)
+VALUES (4444444, 3333333, 'Alex', 'brown',7);
+
+INSERT INTO TravelingWith (TravelingWithID, CustomerID, FirstName, LastName, Age)
+VALUES (5555555, 4444444, 'Ben', 'Stevens',7);
+INSERT INTO TravelingWith (TravelingWithID, CustomerID, FirstName, LastName, Age)
+VALUES (6666666, 4444444, 'Ben', 'Stevens',7);
+
+--BookedTours
+INSERT INTO BookedTour (BookedTourID,PurchaseDate,TravelDate,TotalPrice,TourID,DriverLicense,CustomerID)
+VALUES (1111111111111,'01-JAN-2015','12-FEB-2016',111,1111111,1111111111111,1111111);
+INSERT INTO BookedTour (BookedTourID,PurchaseDate,TravelDate,TotalPrice,TourID,DriverLicense,CustomerID)
+VALUES (2222222222222,'20-FEB-2014','20-FEB-2014',222,2222222,2222222222222,2222222);
+INSERT INTO BookedTour (BookedTourID,PurchaseDate,TravelDate,TotalPrice,TourID,DriverLicense,CustomerID)
+VALUES (3333333333333,'31-MAY-2016','28-FEB-2018',333,3333333,3333333333333,3333333);
+INSERT INTO BookedTour (BookedTourID,PurchaseDate,TravelDate,TotalPrice,TourID,DriverLicense,CustomerID)
+VALUES (4444444444444,'20-OCT-2012','29-APR-2013',444,4444444,4444444444444,4444444);
+INSERT INTO BookedTour (BookedTourID,PurchaseDate,TravelDate,TotalPrice,TourID,DriverLicense,CustomerID)
+VALUES (5555555555555,'12-DEC-2015','30-MAR-2020',555,5555555,5555555555555,4444444);
+INSERT INTO BookedTour (BookedTourID,PurchaseDate,TravelDate,TotalPrice,TourID,DriverLicense,CustomerID)
+VALUES (6666666666666,'15-AUG-2020','15-AUG-2021',666,0000000,5555555555555,6666666);
+INSERT INTO BookedTour (BookedTourID,PurchaseDate,TravelDate,TotalPrice,TourID,DriverLicense,CustomerID)
+VALUES (7777777777777,'15-AUG-2021','16-AUG-2021',666,0000000,5555555555555,6666666);
+
+
+--Traveling
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (1111111, 1111111);
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (1111111, 2222222);
+
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (4444444, 3333333);
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (4444444, 2222222);
+
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (2222222, 1111111);
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (2222222, 3333333);
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (2222222, 4444444);
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (2222222, 5555555);
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (2222222, 2222222);
+
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (3333333, 4444444);
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (3333333, 5555555);
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (3333333, 2222222);
+
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (0000000, 2222222);
+
+INSERT INTO Traveling (TourID, LocationID)
+VALUES (0000000, 1111111);
+
+/* Part 3 */
+/* Queries 1-9 are here. The 10th question is above part 2. */
+
+/* Q1: Report vehicles that can carry more than 10 passengers. */
+SELECT LicensePlate, VehicleMake, VehicleModel 
+FROM Vehicle 
+WHERE MaxPassenger > 10;
+
+/* Q2: For a given tour guide (say TourID = 10), report which of the 5 company 
+vehicles that the tour guide can drive. Assume that the categories are distinct:
+car license permits driving only of cars, bus license only permits driving of buses, etc. */
+SELECT FirstName, LastName, LicensePlate, VehicleMake, VehicleModel
+FROM Vehicle Ve, Guide Gu
+WHERE Ve.VehicleType = Gu.VehicleType
+AND Gu.DriverLicense = 1111111111111;
+
+/* Q3: Report the number of booked tours each customer has. Also include the FirstName, 
+LastName, and Age */
+SELECT Cu.CustomerID, Cu.FirstName, Cu.LastName, Cu.Age, COUNT(Bt.BookedTourID) AS cntBT
+FROM Customer Cu, BookedTour Bt
+WHERE Cu.CustomerID = Bt.CustomerID
+GROUP BY Cu.CustomerID, Cu.FirstName, Cu.LastName, Cu.Age;
+
+/* Q4: Report the number of booked tours to which each tour guide has been assigned. Also 
+include the FirstName, LastName, and Title of the tour guide. */
+SELECT Gu.DriverLicense, Gu.FirstName, Gu.LastName, Gu.Title, COUNT(Bt.BookedTourID) AS cntBT
+FROM Guide Gu, BookedTour Bt
+WHERE Gu.DriverLicense = Bt.DriverLicense
+GROUP BY Gu.DriverLicense, Gu.FirstName, Gu.LastName, Gu.Title;
+
+/* Q5: Report the number of each location type (historic, museum, restaurant) along with 
+the tour name that each tour contains. */
+SELECT TourName, L.LocationType, COUNT(L.LocationID) AS Count
+FROM Tour T, Location L, Traveling Tr
+WHERE T.TourID = Tr.TourID AND L.LocationID = Tr.LocationID
+GROUP BY TourName, L.LocationType
+ORDER BY TourName;
+
+/* Q6: Report the number of people taking each of the different tours.
+Include the TourName, City, State and VehicleType. Make sure to count
+the primary customer as well as those who are traveling along with him or her. */
+
+SELECT TourName, Stateheld, City, VehicleType, COUNT(Tw.CustomerID) AS CustomersOnTour
+FROM TravelingWith Tw, BookedTour B, Tour T
+WHERE B.TourID = T.TourID AND B.CustomerID = Tw.TravelingWithID
+GROUP BY TourName, TravelingWithID, Stateheld, City, VehicleType
+ORDER BY TourName;
+
+/* Q7: Report primary customers who have booked tours in 2015 and 2016. */
+SELECT CustomerID
+FROM BookedTour
+WHERE PurchaseDate >= DATE '2015-01-01'
+AND PurchaseDate <= DATE '2016-12-31';
+
+/* Q8: Report which is the most popular tour, i.e. the tour that has been booked 
+the most often (appears most often in BookedTours). Do not factor in traveling 
+companions of the primary customer. */
+SELECT TourID, Count(BookedTourID) as NumOccur
+FROM BookedTour
+GROUP BY TourID;
+
+/* Report the total number of tours taken by people (primary customers and traveling
+ companions). */
+SELECT Count(*) As TotalNumber
+FROM BookedTour;
